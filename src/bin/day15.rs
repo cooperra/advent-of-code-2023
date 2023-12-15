44150f808391329cc97580f0ebd411a76ed4e71c
Robbie Cooper <cooperra@users.noreply.github.com>
2023-12-15 01:28:41 -0500

    Refactor common code into lib

--- src/bin/day15.rs
+++ src/bin/day15.rs
@@ -1,6 +1,5 @@
+use advent_of_code_2023::day15::*;
 use std::io::{self, BufRead};
-
-type Num = u64;
 
 fn main() {
     let stdin = io::stdin();
@@ -17,25 +16,4 @@ pub fn day15(line: impl AsRef<str>) -> Num {
         sum += chunk_hash as Num;
     }
     sum
-}
-
-fn hash(input: &str) -> u8 {
-    let mut current: Num = 0;
-    for c in input.chars() {
-        let b = c as u8;
-        current += b as Num;
-        current *= 17;
-        current %= 256;
-    }
-    current as u8
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_hash() {
-        assert_eq!(hash(&"HASH"), 52);
-    }
 }
