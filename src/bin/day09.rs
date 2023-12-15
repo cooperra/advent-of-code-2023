1084fa59387039d9040137b520dc1f9b106209f7
Robbie Cooper <cooperra@users.noreply.github.com>
2023-12-15 18:56:58 -0500

    Day 9, part 2

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -8,20 +8,22 @@ fn main() {
     let stdin = io::stdin();
     let lines = stdin.lock().lines().map(|l| l.expect("IO Error"));
     let result = day09(lines);
-    println!("{}", result);
+    println!("{}, {}", result.0, result.1);
 }
 
-fn day09(lines: impl Iterator<Item = impl AsRef<str>>) -> Num {
-    let mut sum = 0;
+fn day09(lines: impl Iterator<Item = impl AsRef<str>>) -> (Num, Num) {
+    let mut sum = (0, 0);
     for line in lines {
-        sum += process_line(line.as_ref());
+        let (prev, next) = process_line(line.as_ref());
+        sum.0 += prev;
+        sum.1 += next;
     }
     sum
 }
 
-fn process_line(line: &str) -> Num {
+fn process_line(line: &str) -> (Num, Num) {
     let nums = parse_line(line);
-    predict_next(&nums)
+    predict_prev_next(&nums)
 }
 
 fn parse_line(line: &str) -> Vec<Num> {
@@ -31,12 +33,15 @@ fn parse_line(line: &str) -> Vec<Num> {
         .collect()
 }
 
-fn predict_next(nums: &Vec<Num>) -> Num {
+fn predict_prev_next(nums: &Vec<Num>) -> (Num, Num) {
     assert!(nums.len() > 0);
     if nums.iter().all(|n| *n == 0) {
-        return 0;
+        return (0, 0);
     }
     let next_row = nums.windows(2).map(|slice| slice[1] - slice[0]).collect();
-    let next_row_prediction = predict_next(&next_row);
-    return nums.last().unwrap() + next_row_prediction;
+    let (next_row_prev, next_row_next) = predict_prev_next(&next_row);
+    return (
+        nums.first().unwrap() - next_row_prev,
+        nums.last().unwrap() + next_row_next,
+    );
 }
