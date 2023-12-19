627f7cd04c923f105ab33ef99cc5b5e6bb9227a4
Robbie Cooper <cooperra@users.noreply.github.com>
2023-12-18 23:05:35 -0500

    Day 17 still not working.

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -1,7 +1,7 @@
 use advent_of_code_2023::cursor_grid::*;
 use advent_of_code_2023::linked_list::LinkedList;
 use std::{
-    cmp::Reverse,
+    cmp::{max, min, Reverse},
     collections::BinaryHeap,
     io::{self, BufRead},
 };
@@ -37,7 +37,8 @@ fn day17(lines: impl Iterator<Item = impl AsRef<str>>) -> Num {
     //     .max()
     //     .unwrap()
     let start_pos = (0, 0);
-    let dest_pos = (grid.rows.len() as i32 - 1, grid.rows[0].len() as i32);
+    let dest_pos = (grid.rows.len() as i32 - 1, grid.rows[0].len() as i32 - 1);
+    assert!(&grid.is_within_bounds(&dest_pos));
     let mut candidate_paths: BinaryHeap<Reverse<HeapEntry>> = [Reverse(HeapEntry(
         manhattan(start_pos, dest_pos),    // heuristic
         0,                                 // path_cost
@@ -47,10 +48,20 @@ fn day17(lines: impl Iterator<Item = impl AsRef<str>>) -> Num {
     ))]
     .into_iter()
     .collect();
+    //let mut visited: HashSet<(Coord, Direction, u8)> = HashSet::new();
     loop {
-        let Reverse(HeapEntry(_, path_cost, current_direction, direction_repeats, path_nodes)) =
-            candidate_paths.pop().unwrap();
+        let Reverse(HeapEntry(
+            heuristic_val,
+            path_cost,
+            current_direction,
+            direction_repeats,
+            path_nodes,
+        )) = candidate_paths.pop().unwrap();
         let current_pos = path_nodes.head().unwrap();
+        println!(
+            "{} {} ({}, {})",
+            heuristic_val, path_cost, current_pos.0, current_pos.1
+        );
         if *current_pos == dest_pos {
             return path_cost;
         }
@@ -58,7 +69,11 @@ fn day17(lines: impl Iterator<Item = impl AsRef<str>>) -> Num {
             if !grid.is_within_bounds(&pos) {
                 return None;
             }
-            if path_nodes.contains(&pos) {
+            //if path_nodes.contains(&pos) {
+            //    return None;
+            //}
+            if dir == current_direction.flipped() {
+                // No U-turns
                 return None;
             }
             let mut new_repeats = direction_repeats;
@@ -71,14 +86,27 @@ fn day17(lines: impl Iterator<Item = impl AsRef<str>>) -> Num {
                 new_repeats = 1;
             }
             let path_append = path_nodes.push(pos);
+            let new_cost = path_cost + *grid.get(&pos) as u32;
             return Some(Reverse(HeapEntry(
-                path_cost + manhattan(pos, dest_pos),
-                path_cost + *grid.get(&pos) as u32,
+                new_cost + heuristic(pos, dest_pos, &grid),
+                new_cost,
                 dir,
                 new_repeats,
                 path_append,
             )));
         });
+
+        // let nexts_tmp: Vec<_> = nexts
+        //     .filter(|Reverse((_, _, dir, dirnim, ll))| {
+        //         !visited.contains((dir, dirnim, ll.head().uwrap()))
+        //     })
+        //     .collect();
+        // visited.extend(
+        //     nexts_tmp
+        //         .iter()
+        //         .map(|Reverse((_, _, dir, dirnim, ll))| (dir, dirnim, ll.head())),
+        // );
+
         candidate_paths.extend(nexts);
     }
 }
@@ -96,6 +124,44 @@ fn neighbor_steps(pos: Coord) -> impl Iterator<Item = Cursor> {
 
 fn manhattan(c1: Coord, c2: Coord) -> u32 {
     (c1.0 - c2.0).abs() as u32 + (c1.1 - c2.1).abs() as u32
+}
+
+fn heuristic(pos: Coord, dest: Coord, grid: &Grid<u8>) -> u32 {
+    let mut sum = 0;
+    // min of each row between pos and dest inclusive
+    // FIXME only works if start above dest
+    let row_first: usize = pos.0 as usize + 1;
+    let row_last: usize = dest.0 as usize;
+    let row_range = row_first..=row_last;
+    if !row_first > row_last {
+        sum += grid
+            .rows
+            .iter()
+            .skip(row_first as usize)
+            .take((row_last + 1 - row_first) as usize)
+            .map(|r| *r.iter().reduce(min).unwrap() as u32)
+            .sum::<u32>();
+    }
+    // min of each column between pos and dest inclusive
+    // FIXME only works if start left of dest
+    let col_first: usize = pos.1 as usize + 1;
+    let col_last: usize = dest.1 as usize;
+    let col_range = col_first..=col_last;
+    if !col_first > col_last {
+        let mut min_each_col = vec![9; col_range.clone().count()]; //Vec::with_capacity(col_last - col_first + 1);
+        for col_idx in col_range {
+            for row_idx in row_range.clone() {
+                //if col_idx == col_first && row_idx == row_first {
+                //    continue;
+                //}
+                let item = grid.rows[row_idx][col_idx];
+                min_each_col[(col_idx - col_first) as usize] =
+                    min(item, min_each_col[(col_idx - col_first) as usize]);
+            }
+        }
+        sum += min_each_col.iter().map(|n| *n as u32).sum::<u32>();
+    }
+    sum
 }
 
 fn parse_grid(lines: impl Iterator<Item = impl AsRef<str>>) -> Grid<u8> {
--- src/linked_list.rs
+++ src/linked_list.rs
@@ -3,6 +3,7 @@ use std::rc::Rc;
 #[derive(Eq, PartialEq)]
 pub struct LinkedList<T> {
     head: Rc<LinkedListNode<T>>,
+    len: usize,
 }
 
 #[derive(Eq, PartialEq)]
@@ -15,6 +16,7 @@ impl<T> LinkedList<T> {
     pub fn new() -> Self {
         Self {
             head: Rc::new(LinkedListNode::<T>::Empty),
+            len: 0,
         }
     }
 
@@ -25,22 +27,40 @@ impl<T> LinkedList<T> {
         }
     }
 
+    pub fn tail(&self) -> Option<Self> {
+        match self.head.as_ref() {
+            LinkedListNode::Empty => None,
+            LinkedListNode::Cons(_, rest) => Some(LinkedList {
+                head: Rc::clone(&rest),
+                len: self.len - 1,
+            }),
+        }
+    }
+
     pub fn push(&self, item: T) -> Self {
         let cons = Rc::new(LinkedListNode::<T>::Cons(item, Rc::clone(&self.head)));
-        Self { head: cons }
+        Self {
+            head: cons,
+            len: self.len + 1,
+        }
     }
 
     pub fn contains(&self, needle: &T) -> bool
     where
         T: Eq,
     {
-        let next_node = &self.head;
-        while let LinkedListNode::Cons(item, next_node) = next_node.as_ref() {
+        let mut current_node = &self.head;
+        while let LinkedListNode::Cons(item, next_node) = current_node.as_ref() {
             if item == needle {
                 return true;
             }
+            current_node = next_node;
         }
         return false;
+    }
+
+    pub fn iter(&self) -> LinkedListIter<T> {
+        LinkedListIter { head: &self.head }
     }
 }
 
@@ -51,5 +71,40 @@ impl<T> FromIterator<T> for LinkedList<T> {
             list = list.push(item);
         }
         list
+    }
+}
+
+pub struct LinkedListIter<'a, T> {
+    head: &'a LinkedListNode<T>,
+}
+
+impl<'a, T> Iterator for LinkedListIter<'a, T> {
+    type Item = &'a T;
+    fn next(self: &mut Self) -> Option<&'a T> {
+        match self.head {
+            LinkedListNode::Empty => None,
+            LinkedListNode::Cons(item, tail) => {
+                self.head = &tail;
+                Some(item)
+            }
+        }
+    }
+}
+
+// impl<T> IntoIterator for LinkedList<T> {
+//     type Item = T;
+//     type IntoIter = LinkedListIter<T>;
+
+//     fn into_iter(self) -> Self::IntoIter {
+//         Self::IntoIter { list: self }
+//     }
+// }
+
+impl<T> Clone for LinkedList<T> {
+    fn clone(&self) -> Self {
+        Self {
+            head: Rc::clone(&self.head),
+            len: self.len,
+        }
     }
 }
