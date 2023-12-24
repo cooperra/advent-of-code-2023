9183b08218f85a8f6e80a42fcfeee1bf732f9e80
Robbie Cooper <cooperra@users.noreply.github.com>
2023-12-24 15:05:34 -0500

    Day 11: convert to sparse grid implementation

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -4,7 +4,7 @@ use std::io::{self, BufRead};
 use advent_of_code_2023::cursor_grid::{manhattan, Coord, Grid};
 
 type Num = u32;
-type Universe = Grid<Node>;
+type Universe = Vec<Coord>;
 type Node = bool;
 
 fn main() {
@@ -21,56 +21,55 @@ fn day11(lines: impl Iterator<Item = impl AsRef<str>>) -> Num {
 }
 
 fn parse_grid(lines: impl Iterator<Item = impl AsRef<str>>) -> Universe {
-    let mut grid = Grid::new();
-    for line in lines {
-        let row: Vec<Node> = line.as_ref().chars().map(|c| c == '#').collect();
-        grid.rows.push(row);
+    let mut universe = Vec::new();
+    for (row_idx, line) in lines.enumerate() {
+        let row_galaxies = line
+            .as_ref()
+            .chars()
+            .enumerate()
+            .filter_map(|(col_idx, c)| (c == '#').then_some((row_idx as i32, col_idx as i32)));
+        universe.extend(row_galaxies);
     }
-    grid
+    universe
 }
 
 fn expand_universe(mut universe: &mut Universe) {
-    // expand cols
-    // Assume all rows same length
-    duplicate_column_if(&mut universe, |c| c.into_iter().all(|x| !*x));
-
-    // expand rows
-    duplicate_item_if(&mut universe.rows, |r: &Vec<Node>| r.iter().all(|x| !*x));
+    let multiplier = 2;
+    replicate_empty_rows(&mut universe, multiplier);
+    replicate_empty_columns(&mut universe, multiplier);
 }
 
-fn duplicate_column_if<T>(grid: &mut Grid<T>, pred: fn(Vec<&T>) -> bool)
-where
-    T: Clone,
-{
-    let matching_indexes: Vec<_> = grid
-        .col_iter()
-        .enumerate()
-        .filter_map(|(idx, item)| pred(item).then_some(idx))
-        .collect();
-    // Reverse ensures we don't invalidate our remaining indexes.
-    for idx in matching_indexes.into_iter().rev() {
-        let new_col: Vec<_> = grid
-            .get_col(idx)
-            .into_iter()
-            .map(|item| item.clone())
-            .collect();
-        grid.insert_col(idx, new_col);
+fn replicate_empty_columns(universe: &mut Universe, multiplier: u32) {
+    universe.sort_by_key(|(_, col_idx)| *col_idx);
+    let mut col_shift: i32 = 0;
+    let mut current_col: i32 = 0;
+    for (_, galaxy_col) in universe.iter_mut() {
+        let col_delta: i32 = *galaxy_col - current_col;
+        if col_delta > 0 {
+            current_col = *galaxy_col;
+            let empty_col_count = col_delta - 1;
+            // When col_delta is 1, this should have no effect, because there are no empty cols.
+            let empty_col_count_expanded = empty_col_count * multiplier as i32;
+            col_shift += empty_col_count_expanded - empty_col_count;
+        }
+        *galaxy_col += col_shift;
     }
 }
 
-fn duplicate_item_if<T>(v: &mut Vec<T>, pred: fn(&T) -> bool)
-where
-    T: Clone,
-{
-    // There's gotta be a better way (a crate for mutating iterators), but the internet stubbornly disagrees.
-    let mut idx = 0;
-    while idx < v.len() {
-        let ref item = v[idx];
-        if pred(item) {
-            v.insert(idx, item.clone());
-            idx += 1;
+fn replicate_empty_rows(universe: &mut Universe, multiplier: u32) {
+    // Assume already sorted by row
+    let mut row_shift: i32 = 0;
+    let mut current_row: i32 = 0;
+    for (galaxy_row, _) in universe.iter_mut() {
+        let row_delta: i32 = *galaxy_row - current_row;
+        if row_delta > 0 {
+            current_row = *galaxy_row;
+            let empty_row_count = row_delta - 1;
+            // When row_delta is 1, this should have no effect, because there are no empty rows.
+            let empty_row_count_expanded = empty_row_count * multiplier as i32;
+            row_shift += empty_row_count_expanded - empty_row_count;
         }
-        idx += 1;
+        *galaxy_row += row_shift;
     }
 }
 
@@ -88,8 +87,5 @@ fn galaxy_pairs(universe: &Universe) -> impl Iterator<Item = Vec<Coord>> + Sized
 }
 
 fn locate_galaxies(universe: &Universe) -> impl Iterator<Item = Coord> + '_ {
-    universe
-        .iter()
-        .zip(universe.positions())
-        .filter_map(|(is_galaxy, pos)| is_galaxy.then_some(pos))
+    universe.clone().into_iter()
 }
