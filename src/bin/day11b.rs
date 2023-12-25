fb56e7fa926dd7ff99efd8c12ebf8967e795874d
Robbie Cooper <cooperra@users.noreply.github.com>
2023-12-25 00:09:45 -0500

    Day 11, part 2

---
+++ src/bin/day11b.rs
@@ -0,0 +1,10 @@
+use std::io::{self, BufRead};
+
+use advent_of_code_2023::day11::day11;
+
+fn main() {
+    let stdin = io::stdin().lock();
+    let lines = stdin.lines().map(|l| l.expect("IO Error"));
+    let result = day11(lines, 1000000);
+    println!("{}", result);
+}
